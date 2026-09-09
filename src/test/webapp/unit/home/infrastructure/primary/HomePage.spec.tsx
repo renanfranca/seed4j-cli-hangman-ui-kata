8969dc9a7933a61e11f4a2e51186adf3b07c4d7e
Renan Franca <renan.andradefranca@gmail.com>
2026-09-09 10:55:42 -0300

    Apply module: react-core

---
+++ src/test/webapp/unit/home/infrastructure/primary/HomePage.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+
+import HomePage from '@/home/infrastructure/primary/HomePage';
+
+describe('Home tests', () => {
+  it('should render without crashing', () => {
+    const { getByText } = render(<HomePage />);
+    const title = getByText('React + TypeScript + Vite');
+    expect(title).toBeTruthy();
+  });
+});
